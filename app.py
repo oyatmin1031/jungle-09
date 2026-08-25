f8dbf5fdf500a5e592f426d0fa71953c396c1485
silverzzo <124555750+silverzzo@users.noreply.github.com>
2026-08-25 17:37:05 +0900

    feat: EC2 MongoDB 연동 및 인증 설정 추가

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client[os.getenv("DB_NAME")]
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    db.posts.insert_one({"title": "로컬에서 EC2로 연결 테스트"})
+    titles = [p["title"] for p in db.posts.find()]
+    return {"posts": titles}
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
